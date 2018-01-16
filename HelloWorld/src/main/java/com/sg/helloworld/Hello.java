85c5e3b5b43e5d98a9a20d2ec7c9081954564590
Phillip Marsley <danielreidnorman@yahoo.com>
2018-01-16 09:34:47 -0600

    DANNORWRK-1 Pre Work

---
+++ HelloWorld/src/main/java/com/sg/helloworld/Hello.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.helloworld;
+
+/**
+ *
+ * @author Dan
+ */
+public class Hello {
+    public static void main(String[] args) {
+        
+        int i = 0;
+        int j = 2;
+        int k = i + j;
+        i = k;
+        
+        System.out.println("Hello, World");
+    }
+}
